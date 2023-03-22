76c242d3c40e58401500fd453669e8f8661f88b7
Annala <n1anje00@students.oamk.fi>
2023-03-22 12:13:52 +0200

    Made pages for categories

---
+++ components/Park.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, ScrollView, Image, StyleSheet, SafeAreaView } from 'react-native';
+import styles from '../style/styles';
+import Header from './Header';
+
+
+
+
+export default Parks = () =>  {
+ 
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      fetch('https://opendata.zoneatlas.com/oulu/objects.json')
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => console.error(error));
+    }, []);
+  
+  return (
+   <View>
+      {data.slice(0, 10).map((object) => (
+        
+        <View key={object.id} >
+          <Text style={styles.title} >{object.title}</Text>
+          <Text >{object.content}</Text>
+          <Text > Koordinaatit: {object.geo.coordinates}</Text>
+          <View key={object.Categories.id}>
+          <Text >{object.Categories.title}</Text>
+
+          {/* <Image source={{ uri: object.Categories.path }} style={styles.image} /> */}
+        </View>
+
+        {object.Categories.map((category) => (
+           <Text key={category.id}>{category.title}</Text>
+          ))}
+
+        {object.Media.map((media) => (
+          <Image key={media.id} source={{ uri: media.path }} style={styles.image} />
+        ))}
+        
+        </View>
+
+      ))}
+    </View>
+  );
+}
