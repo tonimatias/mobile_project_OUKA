54568d930e7748a801fa74875948c93ffdd9789b
Annala <n1anje00@students.oamk.fi>
2023-03-22 12:50:10 +0200

    Fixed parks so they now show

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import Statue from './components/Statue';
 import Fireplace from './components/Fireplace';
 import Frontpage from './components/Frontpage';
+import Architecture from './components/Architecture';
 
 
 const Tab = createBottomTabNavigator();
@@ -22,7 +23,7 @@ export default function App() {
     <Tab.Navigator>
           <Tab.Screen name='Frontpage' component={Frontpage}/>
           <Tab.Screen name='Search' component={App}/>
-          <Tab.Screen name='Categories' component={App} />
+          <Tab.Screen name='Categories' component={Architecture} />
     </Tab.Navigator>
     </NavigationContainer>
   );
