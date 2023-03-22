a3ade99de2c22f15699175bdf8252fb8a470c193
JSKarhu <92786584+JSKarhu@users.noreply.github.com>
2023-03-22 12:38:25 +0200

    lisätty navbar sovellukseen

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, ScrollView, Image, StyleSheet, SafeAreaView } from 'react-native';
 import styles from './style/styles'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import Header from './components/Header'
 import Statue from './components/Statue';
 import Fireplace from './components/Fireplace';
+import Frontpage from './components/Frontpage';
 
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
  
@@ -12,11 +17,18 @@ export default function App() {
 
   return (
     <SafeAreaView>
+    <NavigationContainer>
       <Header />
     <ScrollView >
-      <Statue/>
-      <Fireplace/>
+      <Frontpage />
+
+    <Tab.Navigator>
+          <Tab.Screen name='Frontpage' component={Frontpage}/>
+          <Tab.Screen name='Search' component={App}/>
+          <Tab.Screen name='Categories' component={App} />
+    </Tab.Navigator>
     </ScrollView>
+    </NavigationContainer>
     </SafeAreaView>
   );
 }
