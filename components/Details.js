fa75539deb84e7db87ee5197ba680fbc69ebdbb9
SamiNousiainen <n1nosa00@students.oamk.fi>
2023-03-27 14:53:03 +0300

    yksittäisen kohteen tarkat tiedot

---
+++ components/Details.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Text, ScrollView, Image, BackHandler } from 'react-native';
+import styles from '../style/styles';
+import Header from './Header';
+
+
+
+
+export default Details = ({route,navigation}) =>  {
+ 
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [geo, setGeo] = useState([]);
+  const [media, setMedia] = useState([]);
+    
+
+    useEffect(() => {
+
+        if (route.params?.data) {
+            setTitle(route.params.data.title)
+            setContent(route.params.data.content)
+            setGeo(route.params.data.geo)
+            setMedia(route.params.data.media)
+        }
+
+    BackHandler.addEventListener('hardwareBackPress',close);
+    return () => {
+    BackHandler.removeEventListener('hardwareBackPress',close);
+
+    }
+    }, [route.params?.data]);
+
+    function close() {
+      navigation.goBack(null);
+      return true;
+    }
+
+          return (
+            <ScrollView>
+              <Text style={styles.title}>{title}</Text>
+              <Image
+                  style={styles.image}
+                />
+              <Text style={styles.title1}>{content}</Text>
+              <Text>kartta linkki tähän?{geo.coordinates}</Text>
+            </ScrollView>
+          );
+  }
