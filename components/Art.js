e27e89d4b94b71bb94ec45700f8313fc934e84f4
tonimatias <tonimatias96@gmail.com>
2023-03-31 09:26:27 +0300

    shows only 7 in one page, then continues next page

--- components/Art.js
+++ components/Art.js
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView, Image, StyleSheet, SafeAreaView } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, ScrollView, Image, StyleSheet, SafeAreaView, Button } from 'react-native';
 import styles from '../style/styles';
 import Header from './Header';
 
+export default Art = () => {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0); // new state for total pages
+  const itemsPerPage = 7;
 
+  const scrollViewRef = useRef();
 
+  useEffect(() => {
+    fetch('https://opendata.zoneatlas.com/oulu/objects.json')
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        setTotalPages(Math.ceil(json.length / itemsPerPage)); // calculate total pages
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
-export default Art = () =>  {
- 
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-      fetch('https://opendata.zoneatlas.com/oulu/objects.json')
-        .then((response) => response.json())
-        .then((json) => setData(json))
-        .catch((error) => console.error(error));
-    }, []);
-
-    return (
-      <ScrollView>
-        {data.map((object) => {
-          // Check if the object has the category "Taideteos"
-          const Arts = object.Categories.find(
-            (category) => category.title === "Taideteos"
-          );
+  return (
+    <ScrollView ref={scrollViewRef}>
+      {data.length > 0 &&
+        data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).map((object) => {
+          const Arts = object.Categories.find((category) => category.title === 'Taideteos');
           if (!Arts) {
-            // If the object does not have the "Taideteos" category, skip it
             return null;
           }
           return (
@@ -33,18 +34,34 @@ export default Art = () =>  {
               <Text style={styles.title}>{object.title}</Text>
               <Text style={styles.category_title}>{object.content}</Text>
               <Text style={styles.category_title}>Koordinaatit: {object.geo.coordinates}</Text>
-              <View key={object.Categories.id}>
-              </View>
+              <View key={object.Categories.id}></View>
               {object.Media.map((media) => (
-                <Image
-                  key={media.id}
-                  source={{ uri: media.path }}
-                  style={styles.image}
-                />
+                <Image key={media.id} source={{ uri: media.path }} style={styles.image} />
               ))}
             </View>
           );
         })}
-      </ScrollView>
-    );
-  }
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Button
+          title="Edellinen sivu"
+          disabled={currentPage === 1}
+          onPress={() => {
+            setCurrentPage(currentPage - 1);
+            scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: false });
+          }}
+        />
+        <Text>
+          {currentPage} / 6 {/* display current page and total pages */}
+        </Text>
+        <Button
+          title="Seuraava sivu"
+          disabled={currentPage === 6} 
+          onPress={() => {
+            setCurrentPage(currentPage + 1);
+            scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: false });
+          }}
+        />
+      </View>
+    </ScrollView>
+  );
+}
