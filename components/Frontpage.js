d62b38c20fdfe6f69dcb0df13a6840ba844056bc
BENJAMIN_MADETOJA <benjaminmadetoja@gmail.com>
2023-04-12 10:28:04 +0300

    Frontpage

--- components/Frontpage.js
+++ components/Frontpage.js
@@ -10,11 +10,12 @@ export default Frontpage = (props) =>  {
   
   
     <View style={{ ...styles.frontpage_container, backgroundColor: isDarkmode ? styles.contentBackgroundDark.backgroundColor : styles.contentBackgroundLight.backgroundColor }}>
-     <ImageBackground source={require('../pictures/frontpage_img_2.jpg')} resizeMode="cover" style={styles.image}>
-       
-      <Text style={styles.frontpage_title}>Oulu Awaits</Text>
+     <ImageBackground source={require('../pictures/frontpage_img_2.jpg')} resizeMode="cover" style={styles.imagefrontpage}>
+       <View style={styles.container1}>
+      <Text style={styles.frontpage_title}>Oulu Awaits!</Text>
       <Text style={styles.frontpage_txt}>OuluAwaits on sovellus, jonka avulla löydät mielenkiintoista arkkitehtuuria, taideteoksia, patsaita, sekä puistoja ja historiallisia paikkoja.  </Text>
       <Text style={styles.frontpage_txt}>Alhaalta voit etsiä aluetta, taiteilijaa, tai paikkaa. Valikosta löytyy jokaiselle kategorialle oma sivusto.</Text>
+      </View>
       </ImageBackground> 
     </View>
    
