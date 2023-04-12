2c873f6414237cdf6b8d1f59f9e791226a5be691
JSKarhu <92786584+JSKarhu@users.noreply.github.com>
2023-04-12 12:15:06 +0300

    fixed misspells because i suck

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -11,7 +11,7 @@ export default AboutUs = () =>  {
         <Image source={Oulunvaakuna} style={styles.OuluSeal} resizeMode='contain'/>
         </View>
       <Text style={styles.title}>Terve</Text>
-      <Text style={styles.content}>Teimme OuluAwaits sovelluksen Oulun historiasta kiinnostuneille ihmisille. Olitpa sitten ensimmäistä kertaa käymässä tai sitten kaupungin oma asukki, Oulu tarjoaa monia kiinnostavia nähtävyyksiä ja historiallisia kohteita joista jokainen voi nauttia. Sovellus löydät sijainnin nähtävyyksille, sekä nähtävyyden historiaa (jos tieto on saatavilla). Tämän lisäksi löydät sijainnit yleisille nuotiopaikoille jonne voit mennä vaikka makkaranpaistoon.  </Text>
+      <Text style={styles.content}>Teimme OuluAwaits sovelluksen Oulun historiasta kiinnostuneille ihmisille. Olitpa sitten ensimmäistä kertaa käymässä tai sitten kaupungin oma asukki, Oulu tarjoaa monia kiinnostavia nähtävyyksiä ja historiallisia kohteita joista jokainen voi nauttia. Sovellus tarjoaa sijainnit nähtävyyksille, sekä antaa sinulle niiden historian (jos tieto on saatavilla). Tämän lisäksi löydät sijainnit yleisille nuotiopaikoille jonne voit mennä vaikka makkaranpaistoon.  </Text>
       
     </ScrollView>
    
