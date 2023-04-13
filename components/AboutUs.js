cf83bf13d4bd33a7d1aacc9973ad91747a2ea796
JSKarhu <92786584+JSKarhu@users.noreply.github.com>
2023-04-13 11:35:47 +0300

    changed title for aboutUs

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -10,7 +10,7 @@ export default AboutUs = () =>  {
        <View>
         <Image source={require('../pictures/Oulunvaakuna.png')} style={styles.OuluSeal} resizeMode='contain'/>
       </View>
-      <Text style={styles.title}>Terve</Text>
+      <Text style={styles.title}>Info</Text>
       <Text style={styles.content}>Teimme OuluAwaits sovelluksen Oulun historiasta kiinnostuneille ihmisille. Olitpa sitten ensimmäistä kertaa käymässä tai sitten kaupungin oma asukki, Oulu tarjoaa monia kiinnostavia nähtävyyksiä ja historiallisia kohteita joista jokainen voi nauttia. Sovellus tarjoaa sijainnit nähtävyyksille, sekä antaa sinulle niiden historian (jos tieto on saatavilla). Tämän lisäksi löydät sijainnit yleisille nuotiopaikoille jonne voit mennä vaikka makkaranpaistoon.  </Text>
       
     </ScrollView>
