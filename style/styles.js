a59dd13475376e7b7af1df28b51c457b6af5ddff
Annala <n1anje00@students.oamk.fi>
2023-04-27 10:05:24 +0300

    change

--- style/styles.js
+++ style/styles.js
@@ -17,7 +17,7 @@ export default StyleSheet.create({
       flexDirection: 'row',
       alignSelf: 'center',
       flex: 1,
-      padding: 19,
+     
     
   
     },
@@ -53,8 +53,6 @@ export default StyleSheet.create({
       fontFamily: 'ManropeBold',
       color:'white',
       
-      
-    
     },
 
     frontpage_txt: {
