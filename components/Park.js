a7c8041e3cbe9848a9327136a76f7e8cb4a07219
JSKarhu <92786584+JSKarhu@users.noreply.github.com>
2023-04-27 10:24:10 +0300

    Fixed puistot page

--- components/Park.js
+++ components/Park.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, ScrollView, Image, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, Image, StyleSheet, SafeAreaView, TouchableOpacity, FlatList, Modal, Button, Pressable } from 'react-native';
 import styles from '../style/styles';
-import Header from './Header';
 import { useNavigation } from '@react-navigation/native';
+import { Details2 } from './Details2';
+import { object } from 'prop-types';
+import { Ionicons } from '@expo/vector-icons';
 
-
-export default Park = ({mode}) => {
+export default Park = ({ mode }) => {
   const [data, setData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0); // new state for total pages
+  const [totalPages, setTotalPages] = useState(0);
   const itemsPerPage = 7;
+
+  const [showModal, setShowModal] = useState(false);
+  const [selectedObject, setSelectedObject] = useState(null);
 
   const navigation = useNavigation();
 
-
   const scrollViewRef = useRef();
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  }
 
   useEffect(() => {
     fetch('https://opendata.zoneatlas.com/oulu/objects.json')
@@ -25,19 +31,56 @@ export default Park = ({mode}) => {
           return !!Parks;
         });
         setData(parkObjects);
-        setTotalPages(Math.ceil(parkObjects.length / itemsPerPage)); // calculate total pages based on filtered objects
+        setTotalPages(Math.ceil(parkObjects.length / itemsPerPage));
       })
       .catch((error) => console.error(error));
   }, []);
 
-  useEffect(() =>{
-  },[mode]);
+  useEffect(() => {
+    console.log('isDarkmode: ' + mode);
+  }, [mode]);
+
+  const handleScroll = ({ nativeEvent }) => {
+    if (isCloseToBottom(nativeEvent)) {
+      loadMoreData();
+    }
+  };
+
+  const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
+    const paddingToBottom = 20;
+    return layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom;
+  };
+
+  const loadMoreData = () => {
+    const currentPage = Math.ceil(data.length / itemsPerPage);
+    if (currentPage < totalPages) {
+      const start = currentPage * itemsPerPage;
+      const end = start + itemsPerPage;
+      fetch(`https://opendata.zoneatlas.com/oulu/objects.json?start=${start}&end=${end}`)
+        .then((response) => response.json())
+        .then((json) => {
+          const parkObjects = json.filter((item) => {
+            const Parks = item.Categories.find((category) => category.title === 'Puisto');
+            return !!Parks;
+          });
+          setData([...data, ...parkObjects]);
+        })
+        .catch((error) => console.error(error));
+    }
+  };
+  
 
   return (
-    <ScrollView style={{backgroundColor: mode ? styles.backgroundDark.backgroundColor : styles.backgroundLight.backgroundColor}} ref={scrollViewRef}>
-      {data.length > 0 &&
-        data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).map((object) => {
-          Parks = object.Categories.find((category) => category.title ==='Puisto');
+    <View style={{ flex: 1 }}>
+      <FlatList
+        style={{ backgroundColor: mode ? styles.backgroundDark.backgroundColor : styles.backgroundLight.backgroundColor
+ }}
+        ref={scrollViewRef}
+        data={data}
+        renderItem={({ item }) => {
+          const Parks = item.Categories.find(
+            (category) => category.title === 'Puisto'
+          );
           if (!Parks) {
             return null;
           }
@@ -47,54 +90,61 @@ export default Park = ({mode}) => {
               <Image style={styles.image}source={require('../pictures/placeholder.png')}></Image>
             </View>
           );
-
+  
           return (
-            <View key={object.id} style={[{
-              borderBottomColor: mode ? styles.bgDark.borderBottomColor   : styles.bgLight.borderBottomColor,
-              borderBottomWidth: mode ? styles.bgDark.borderBottomWidth : styles.bgLight.borderBottomWidth,
-              backgroundColor: mode? styles.contentBackgroundDark.backgroundColor : styles.contentBackgroundLight.backgroundColor,
-              padding: mode ? styles.bgDark.padding : styles.bgLight.padding }]}>
-              <Text style={{...styles.category_title, color: mode ? styles.darkColor.color : styles.lightColor.color}}>{object.title.toUpperCase()}</Text>
-            {object.Media.length > 0 ? (
-                object.Media.map((media) => (
-                <Image key={media.id} source={{ uri: media.path }} style={styles.image} />
-                ))
-            ) : (
-                <PlaceholderImage />
-            )}
-            
-              <View key={object.Categories.id}></View>
-            <TouchableOpacity  style={styles.Button} title='lisätietoa' onPress={() => navigation.navigate('Lisätiedot', {data: object})}>
-              <Text style={styles.buttonText}>Lisätietoja</Text>
-            </TouchableOpacity>
+            <View
+              key={item.id}
+              style={[
+                styles.bg,
+                {
+                  backgroundColor: mode
+                    ? styles.contentBackgroundDark.backgroundColor
+                    : styles.contentBackgroundLight.backgroundColor,
+                },
+              ]}
+            >
+              <Text style={styles.category_title}>
+                {item.title.toUpperCase()}
+              </Text>
+              {item.Media.map((media) => (
+                <Image
+                  key={media.id} 
+                  source={{ uri: media.path }}
+                  style={styles.image}
+                />
+              ))}
+              <View key={item.Categories.id}></View>
+              <TouchableOpacity
+                style={styles.Button}
+                title='lisätietoa'
+                onPress={() => {
+                setSelectedObject(item);
+                toggleModal();
+                }}>
+                <Text style={styles.buttonText}>Lisätietoja</Text>
+              </TouchableOpacity>
+
+              <Modal visible={showModal} animationType="none">
+                <SafeAreaView style={[styles.bg, {backgroundColor: mode ? styles.contentBackgroundDark.backgroundColor : styles.contentBackgroundLight.backgroundColor}]}>
+                <Pressable style={{...styles.returnButton, backgroundColor: mode ? styles.contentBackgroundDark.backgroundColor : styles.contentBackgroundLight.backgroundColor}} onPress={toggleModal}>
+                <Ionicons style={styles.arrowIcon} size={35} color='#9600AE' name="arrow-back-outline"/>
+                </Pressable>
+                {selectedObject && (
+                  <Details2 object={selectedObject} />
+                )}
+                
+                </SafeAreaView>
+              </Modal>
             </View>
           );
-        })}
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          <TouchableOpacity
-            title="Edellinen sivu"
-            style={styles.category_button}
-            disabled={currentPage === 1}
-            onPress={() => {
-              setCurrentPage(currentPage - 1);
-              scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: false });
-            }}>
-            <Text style={styles.category_buttonText}>Edellinen sivu</Text>
-          </TouchableOpacity>
-          <Text style={styles.page_number}>
-            {currentPage} / {totalPages} {/* display current page and total pages */}
-          </Text>
-          <TouchableOpacity
-            title="Seuraava sivu"
-            style={styles.category_button}
-            disabled={currentPage === totalPages} 
-            onPress={() => {
-              setCurrentPage(currentPage + 1);
-              scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: false });
-            }}>
-            <Text style={styles.category_buttonText}>Seuraava sivu</Text>
-          </TouchableOpacity>
-      </View>
-    </ScrollView>
-  );
-}
+        }}
+        keyExtractor={(item) => item.id.toString()}
+        onEndReached={loadMoreData}
+        onEndReachedThreshold={0.5}
+        initialNumToRender={itemsPerPage}
+        maxToRenderPerBatch={itemsPerPage}
+        windowSize={10}
+      />
+    </View>
+  )};  
+
